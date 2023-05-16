b637eb388710c65f20d6e7e79b109f9a60dddeb1
Zhujingxi <3305546697@qq.com>
2023-05-16 20:17:51 +0800

    Update main.py
    Optimazied code

--- src/main.py
+++ src/main.py
@@ -1,33 +1,22 @@
 import itertools
 
-list = (open("List.txt", "r"))
+with open("List.txt", "r") as f:
+    standard_resistor = [float(value.strip('\n')) for value in f if value.strip('\n') != 'end']
+
 target = float(input("target value: "))
 num = int(input("number: "))
-
-standard_resistor = []
-while 1:
-    value = list.readline().strip('\n')
-    if value == 'end':
-        break
-    standard_resistor.append(float(value))
 
 if target in standard_resistor:
     print(target)
     if input("Press N if you don't have: ") != "N":
         exit()
 
-
 def cal_resistor(resistor_value):
-    reciprocal_resistor_value = []
-    for y in resistor_value:
-        reciprocal_resistor_value.append(1 / y)
-    v = 0
-    for x in reciprocal_resistor_value:
-        v += x
+    reciprocal_resistor_value = [1 / y for y in resistor_value]
+    v = sum(reciprocal_resistor_value)
     v = round(1 / v, 6)
     error_persent = str(round((target - v) / target * 100, 2)) + "%"
     return [resistor_value, v, error_persent]
-
 
 i = 0
 for v in standard_resistor:
@@ -35,12 +24,9 @@ for v in standard_resistor:
         break
     i += 1
 
-# reciprocal_standard_resistor = []
 standard_resistor = standard_resistor[i:-1]
 
-combined = []
-for p in itertools.combinations_with_replacement(standard_resistor, num):
-    combined.append(cal_resistor(p))
+combined = [cal_resistor(p) for p in itertools.combinations_with_replacement(standard_resistor, num)]
 
 reciprocal_target = 1 / target
 
